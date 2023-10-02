d66e11009007b49cde7d2049cce5464ca2327d87
jhaayushofficial <115531575+jhaayushofficial@users.noreply.github.com>
2023-10-03 01:37:31 +0530

    Create add_without_sum.cpp

---
+++ C++/add_without_sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Add(int x, int y)
+{
+	if (y == 0)
+		return x;
+	else
+		return Add( x ^ y,(unsigned) (x & y) << 1);
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout << Add(a, b);
+    return 0;
+}
