09afab5d52d86bcf101b3e0d8f3f0a3068504871
Divyansh Asthana <shivammohan850@gmail.com>
2023-10-02 22:56:41 +0530

    Updated Valid Anagram

--- C++/Valid Anagram.cpp
+++ C++/Valid Anagram.cpp
@@ -1,19 +1,21 @@
-//using hash table + string
+//using HashMap
 bool isAnagram(string s,string t){
-	if(s.length()!=t.length()){
-		return false;
-	}
-	
-	vector<int>freq(26,0);
-	for(int i=0;i<s.length();i++){
-		freq[s[i]-'a']++;
-		freq[t[i]-'a']++;
-	}
-	
-	for(int i=0;i<freq.size();i++){
-		if(freq[i]!=0){
-			return false;
-		}
-	}
-	return true;
+	unordered_map<char,int> mp;
+        //increment freq for s
+        for(auto i: s){
+            mp[i]++;
+        } 
+        //decrement freq for t
+        for(auto i: t){
+            mp[i]--;
+        } 
+
+        for(auto c: mp){
+            if(c.second != 0) return false;
+        }
+        return true;
+
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
 }
