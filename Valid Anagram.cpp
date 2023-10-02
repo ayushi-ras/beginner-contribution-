88f8956fefedf30b6a74f5d46d3251a63b22b148
Divyansh Asthana <shivammohan850@gmail.com>
2023-10-02 19:16:31 +0530

    More easy approach using HashMap

--- Valid Anagram.cpp
+++ Valid Anagram.cpp
@@ -1,19 +1,21 @@
-//using hash table + string
-bool isAnagram(string s,string t){
-	if(s.length()!=t.length()){
-		return false;
-	}
-	
-	vector<int>freq(26,0);
-	for(int i=0;i<s.length();i++){
-		freq[s[i]-'a']++;
-		freq[t[i]-'a']++;
-	}
-	
-	for(int i=0;i<freq.size();i++){
-		if(freq[i]!=0){
-			return false;
-		}
-	}
-	return true;
+//using hash map
+bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp;
+        //increment freq for s
+        for(int i=0; i<s.length(); i++){
+            mp[s[i]]++;
+        }
+        //decrement freq for t
+        for(int i=0; i<t.length(); i++){
+            mp[t[i]]--;
+        }
+        for(auto i: mp){
+            if(i.second != 0) return false;
+        }
+        return true;
+        
+        // optional method
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
 }
